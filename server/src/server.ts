7b3db3b195dd4b07665fc91b6d37c855b80c65c0
RudraKo <punar.vashu@adypu.edu.in>
2026-04-23 00:52:50 +0530

    fix: call app.listen() to actually start HTTP server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,12 @@
 import { createApp } from "./app";
+import { env } from "./config/env";
 
 export const app = createApp();
+
+if (require.main === module) {
+  app.listen(env.port, () => {
+    console.log(`[INFO] Server is running on http://localhost:${env.port}`);
+  });
+}
+
 export default app;
